c88b293ca2dfc8483911cb4bf8993b615fc80957
Hidden Corner <Hidden_Corner@outlook.com>
2022-08-26 22:03:35 +0800

    添加项目文件。

---
+++ GuFengApi/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace GuFengApi
+{
+    public class Book
+    {
+        string title;
+        Uri cover;
+        string updateTo; // 更新到哪个章节
+        string time; // 最后更新时间
+        Uri bookUri;
+
+        public void DownloadTo(string path)
+        {
+
+        }
+
+        public void DownloadSinglePageTo(int chapter, int page, string path)
+        {
+
+        }
+
+        // 初始化相关，应给 Client 写入权限
+        public string Title { get => title; internal set => title = value; }
+        public Uri Cover { get => cover; internal set => cover = value; }
+        public string UpdateTo { get => updateTo; internal set => updateTo = value; }
+        public string Time { get => time; internal set => time = value; }
+        internal Uri BookUri { get => bookUri; set => bookUri = value; }
+    }
+}
---
+++ GuFengApi/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace GuFengApi
+{
+    public class Client
+    {
+        protected static string mainUri = "https://www.123gf.com/";
+
+        public Book[] Search(string title)
+        {
+            // 获取基本信息
+            HtmlDocument doc = GetDocument(new Uri(mainUri + "search/?keywords=" + title));
+            HtmlNodeCollection bookRootNodes = doc.DocumentNode.SelectNodes("//ul[@class='book-list clearfix']/li[@class='item-lg']");
+            Book[] books = new Book[bookRootNodes.Count];
+
+            //初始化 books
+            for(int i = 0; i < bookRootNodes.Count; ++i)
+            {
+                books[i] = new Book();
+                books[i].Title = bookRootNodes[i].SelectSingleNode("/a").Attributes["title"].ToString();
+                books[i].Cover = new Uri(bookRootNodes[i].SelectSingleNode("/a/img").Attributes["src"].ToString());
+                books[i].BookUri = new Uri(bookRootNodes[i].SelectSingleNode("/a").Attributes["href"].ToString());
+                books[i].UpdateTo = bookRootNodes[i].SelectSingleNode("/a/span[@class='tt']").InnerText;
+                books[i].Time = bookRootNodes[i].SelectSingleNode("/span").InnerText;
+            }
+
+            return books;
+        }
+
+        protected HttpWebRequest InitApiHttpRequest(Uri requestUri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUri);
+            request.Method = "GET";
+            request.UserAgent = "GuFengViewerLib/1.0";
+            request.KeepAlive = false;
+            request.Referer = "https://www.123gf.com/";
+            return request;
+        }
+
+        protected HtmlDocument GetDocument(Uri htmlUri)
+        {
+            HttpWebRequest request = InitApiHttpRequest(htmlUri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(reader.ReadToEnd());
+            reader.Close();
+            stream.Close();
+            response.Close();
+            return doc;
+        }
+    }
+}
